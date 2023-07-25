f15efd8dd10ebd2bb5b0e1dc4b3aeb23214bcc99
minjunsz <minjunsz@postech.ac.kr>
2023-07-25 13:52:06 +0000

    add rss feed button

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,6 +1,7 @@
 import { PERSONAL_INFO, BLOG_INFO } from '@/data/blogMetaData'
 import Link from 'next/link'
 import SocialIcon from './SocialIcons'
+import { MdRssFeed } from 'react-icons/md'
 
 export default function FooterSection() {
   return (
@@ -11,14 +12,20 @@ export default function FooterSection() {
           <SocialIcon kind="github" href={PERSONAL_INFO.github} size="1.5rem" />
           <SocialIcon kind="discord" href={PERSONAL_INFO.discord} size="1.5rem" />
         </div>
-        <div className="mb-8 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-3 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{BLOG_INFO.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{BLOG_INFO.siteTitle}</Link>
         </div>
+        <div className="mb-8 flex items-center text-sm text-gray-500 dark:text-gray-400">
+          <MdRssFeed size="1.5rem" />
+          <Link href="/rss.xml" className='px-1' > rss 2.0</Link>
+          <Link href="/rss.json" className='px-1 border-l border-gray-500 dark:border-gray-400' > json </Link>
+          <Link href="/atom.xml" className='px-1 border-l border-gray-500 dark:border-gray-400' > atom 1.0</Link>
+        </div>
       </div>
-    </footer>
+    </footer >
   )
 }
